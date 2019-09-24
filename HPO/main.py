26919e81e06ae5a417b521ea7d00022a3ac6e8f0
ericharper <complex451@gmail.com>
2019-09-24 10:37:43 -0600

    added num_particles to argparser

--- HPO/main.py
+++ HPO/main.py
@@ -20,19 +20,30 @@ def parse_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
     # data generation arguments
-    parser.add_argument('--coil_type', default='helix', type=str, help='the type of coil to generate the data')
-    parser.add_argument('--num_blobs', default=1000, type=int, help='the number of blobs generated on the GPU')
-    parser.add_argument('--num_coordinates', default=400, type=int, help='the number of starting locations of each blob')
-    parser.add_argument('--sdev_scale', default=.3, type=float, help='standard deviation of normals used to generate data')
-    parser.add_argument('--noise_scale', default=.1, type=float, help='additional noise')
-    parser.add_argument('--coil_density', default=12.0, type=float, help='how tight the coils are')
+    parser.add_argument('--coil_type', default='helix', type=str,
+                        help='the type of coil to generate the data')
+    parser.add_argument('--num_blobs', default=1000, type=int,
+                        help='the number of blobs generated on the GPU')
+    parser.add_argument('--num_coordinates', default=400, type=int,
+                        help='the number of starting locations of each blob')
+    parser.add_argument('--sdev_scale', default=.3, type=float,
+                        help='standard deviation of normals used to generate data')
+    parser.add_argument('--noise_scale', default=.1, type=float,
+                        help='additional noise')
+    parser.add_argument('--coil_density', default=12.0, type=float,
+                        help='how tight the coils are')
     
     # ETL arguments
-    parser.add_argument('--train_test_overlap', default=.05, help='percentage of train and test distribution that overlaps')
+    parser.add_argument('--train_test_overlap', default=.05,
+                        help='percentage of train and test distribution that overlaps')
     
     # HPO arguments
-    parser.add_argument('--num_gpus', default=1, type=int, help='the number of gpus to use')
-    parser.add_argument('--num_timesteps', default=10, type=int, help='the number of timesteps to run HPO')
+    parser.add_argument('--num_gpus', default=1, type=int,
+                        help='the number of gpus to use')
+    parser.add_argument('--num_timesteps', default=10, type=int,
+                        help='the number of timesteps to run HPO')
+    parser.add_argument('--num_particles', default=32, type=int,
+                        help='the number of particles in the swarm')
     
     args = parser.parse_args()
     
@@ -53,15 +64,15 @@ def main(args):
                                              plotFlag = False)
     
     # split
-    trainData_cDF, trainLabels_cDF, testData_cDF, testLabels_cDF, t_split = rl.split_train_test_nfolds ( data, labels, trainTestOverlap = args.train_test_overlap)
+    trainData_cDF, trainLabels_cDF, testData_cDF, testLabels_cDF, t_split = \
+        rl.split_train_test_nfolds ( data, labels, trainTestOverlap = args.train_test_overlap)
 
     # apply standard scaling
     trainMeans, trainSTDevs, t_scaleTrain = rl.scale_dataframe_inplace ( trainData_cDF )
-    _,_, t_scaleTest = rl.scale_dataframe_inplace ( testData_cDF, trainMeans, trainSTDevs )
+    _, _, t_scaleTest = rl.scale_dataframe_inplace ( testData_cDF, trainMeans, trainSTDevs )
     
     # launch HPO on Dask
-    # TODO: argument parser nParticles
-    nParticles = 32
+    nParticles = args.num_particles
     
     # TODO: add ranges to argparser
     paramRanges = { 0: ['max_depth', 3, 15, 'int'],
@@ -69,11 +80,16 @@ def main(args):
                     2: ['lambda', 0, 1, 'float'] }
     
     accuracies, particles, velocities, particleSizes, particleColors, \
-    bestParticleIndex, bestParamIndex, particleBoostingRounds, trainingTimes, _, elapsedTime = rl.run_hpo(client, args.num_timesteps, 
-                                                                                                          nParticles,
-                                                                                                          paramRanges, 
-                                                                                                          trainData_cDF, trainLabels_cDF, testData_cDF, testLabels_cDF,
-                                                                                                          plotFlag=False)
+    bestParticleIndex, bestParamIndex, particleBoostingRounds, \
+    trainingTimes, _, elapsedTime = rl.run_hpo(client,
+                                               args.num_timesteps,
+                                               nParticles,
+                                               paramRanges,
+                                               trainData_cDF,
+                                               trainLabels_cDF,
+                                               testData_cDF,
+                                               testLabels_cDF,
+                                               plotFlag=False)
     
     # TODO: Scaling up: DGX-1, DGX2, Scaling out: Slurm, K8s 
     
