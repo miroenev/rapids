02ad3e24aca714ef1e64af50448acc66ec9732ca
m <miro.enev@gmail.com>
2019-12-07 05:28:55 -0800

    cleaning up

--- HPO/data/fmnist/README.md
+++
@@ -1,1 +0,0 @@
-# https://github.com/zalandoresearch/fashion-mnist
--- HPO/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='rapids_lib',
-      version='11.0',
-      description='Helper RAPIDS wrapper',
-      py_modules=['rapids_lib_v11'] 
-     )
