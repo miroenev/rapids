39de6d5cdda9d4762766215fa157ae05ea04b76f
ericharper <complex451@gmail.com>
2019-12-12 17:36:40 -0700

    added visualization to setup.py

--- HPO/setup.py
+++ HPO/setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='rapids-dask-hpo',
       version='0.0',
       description='Particle Swarm HPO',
-      py_modules=['swarm', 'data_utils'] 
+      py_modules=['swarm', 'data_utils', 'visualization'] 
      )
